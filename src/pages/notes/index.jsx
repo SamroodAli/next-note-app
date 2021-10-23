a7fbf5a9e153cda6c59d1d583ac83add45b38100
samroodAli <devsamrood@gmail.com>
2021-10-23 13:06:48 +0530

    Notes:Create notes index page using pages/notes

---
+++ src/pages/notes/index.jsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+const Index = () => {
+  const notes = new Array(15)
+    .fill(1)
+    .map((e, i) => ({ id: i, title: `This is my note ${i}` }));
+
+  return (
+    <div sx={{ variant: "containers.page" }}>
+      <h1>My Notes</h1>
+
+      <div
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          flexWrap: "wrap",
+        }}
+      >
+        {notes.map((note) => (
+          <div sx={{ width: "33%", p: 2 }} key={note.id}>
+            <Link href="/notes/[id]" as={`/notes/${note.id}`}>
+              <a sx={{ textDecoration: "none", cursor: "pointer" }}>
+                <div sx={{ variant: "containers.card" }}>
+                  <strong>{note.title}</strong>
+                </div>
+              </a>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Index;
