70ffe444b29d5d807dcef67f21326b0798b6eb66
samroodAli <devsamrood@gmail.com>
2021-10-23 13:08:12 +0530

    DynamicPage:Use [] to create a note id page

---
+++ src/pages/notes/[id].jsx
@@ -0,0 +1,15 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+const NoteId = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  return (
+    <div sx={{ variant: "containers.page" }}>
+      <h1>Note: {id} </h1>
+    </div>
+  );
+};
+
+export default NoteId;
